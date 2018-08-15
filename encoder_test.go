677997e960f28ee94af5c253c7ec3d49d14ff793
Jonathan A. Sternberg <jonathan@influxdata.com>
2018-08-15 15:36:04 -0500

    Add the stacktrace as an additional field if it is present
    The stacktrace will be added the same as if it were added by
    `zap.Stack("stacktrace")` and will perform the same escape sequences
    that were taken from the JSON escaping code.

--- encoder_test.go
+++ encoder_test.go
@@ -149,3 +149,43 @@ func TestEncodeCaller(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "caller=h2g2.go:42 k=v\n", buf.String())
 }
+
+func TestEncodeStacktrace(t *testing.T) {
+	enc := &logfmtEncoder{buf: bufferpool.Get(), EncoderConfig: &zapcore.EncoderConfig{
+		EncodeTime:     zapcore.EpochTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	}}
+
+	var buf *buffer.Buffer
+	var err error
+	encodeEntry := func() {
+		buf, err = enc.EncodeEntry(
+			zapcore.Entry{
+				Level:      zapcore.DebugLevel,
+				Time:       time.Time{},
+				LoggerName: "test",
+				Message:    "stacktrace test",
+				Stack: `panic: an unexpected error occurred
+
+goroutine 1 [running]:
+main.main()
+		/go/src/github.com/jsternberg/myawesomeproject/h2g2.go:4 +0x39
+`,
+			},
+			[]zapcore.Field{
+				zap.String("k", "v"),
+			},
+		)
+	}
+
+	encodeEntry()
+	assert.Nil(t, err)
+	assert.Equal(t, "k=v\n", buf.String())
+
+	enc.truncate()
+	enc.EncoderConfig.StacktraceKey = "stacktrace"
+	encodeEntry()
+	assert.Nil(t, err)
+	assert.Equal(t, `k=v stacktrace="panic: an unexpected error occurred\n\ngoroutine 1 [running]:\nmain.main()\n\t\t/go/src/github.com/jsternberg/myawesomeproject/h2g2.go:4 +0x39\n"
+`, buf.String())
+}
